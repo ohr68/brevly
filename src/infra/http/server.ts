37f1491d3ebf9a7f3a56bf0c5b0c8225d42a5004
Otávio Odenik <otavio.odenik@etec.sp.gov.br>
2025-09-22 21:07:23 -0300

    feat: :construction: Adiciona estrutura do banco de dados

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,10 +1,47 @@
-import fastifyCors from "@fastify/cors";
-import fastify from "fastify";
+import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import fastify from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { env } from '@/env'
 
 const server = fastify()
 
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, _request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.validation,
+    })
+  }
+
+  console.error(error)
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
+
 server.register(fastifyCors, { origin: '*' })
 
-server.listen({port: 3333, host: '0.0.0.0'}).then(() => {
-    console.log('🚀 HTTP Server Running!!')
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Brev.ly Server',
+      version: '1.0.0',
+    },
+  },
+})
+
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+server.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
+  console.log('🚀 HTTP Server Running!!')
 })
---
+++ src/infra/schemas/urls.ts
@@ -0,0 +1,12 @@
+import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+import { uuidv7 } from 'uuidv7'
+
+export const urls = pgTable('urls', {
+    id: text('id')
+    .primaryKey()
+    .$defaultFn(() => uuidv7()),
+    originalUrl: text('orignal_url').notNull(),
+    shortenedUrl: text('shortened_url').notNull().unique(),
+    accessCount: integer('access_count'),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+})
